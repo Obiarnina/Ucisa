b2ebb36617ae614b7685bc1358885aaadcc0577c
servicemi <52180716+servicemi@users.noreply.github.com>
2019-11-12 20:39:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ sudo mv /tmp/$tmpfoldername/defaultchecker.sh /tmp/$tmpfoldername/$checker.sh
 ##########################################################
 #windscribe install
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
-echo 'deb https://repo.windscribe.com/ubuntu zesty main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
+echo 'deb https://repo.windscribe.com/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
 sudo apt-get update
 sudo apt-get install -y windscribe-cli
 #windscribe connect
