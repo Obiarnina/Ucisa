1ac0f4616eaac3281599046c3769ffc4160683ab
servicemi <52180716+servicemi@users.noreply.github.com>
2019-11-13 05:06:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ sudo apt-get install libmicrohttpd10 -y
 sudo mkdir ~/.cloudshell
 sudo touch ~/.cloudshell/no-apt-get-warning
 sudo apt-get install dos2unix -y
-#sudo apt-get install apt-transport-https ca-certificates
 ID="$(hostname)"
 THREADS=$(nproc --all)
 reboot_time=$(shuf -i 10-18 -n 1)
+cont_v=$(shuf -e US-C US US-M CA AT HR CY CZ DK FI FR GR HU IL IT MD NO PL GB ZA AU TR AU ID SG KR AR MX -n 1)
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 echo 'sudo reboot -f' | at now + $reboot_time minutes
 timer=$(gpw 1 11)
@@ -50,14 +50,16 @@ sudo sed -i "s/\<defaulttimer.sh\>/$timer.sh/g" /tmp/$tmpfoldername/defaultcheck
 sudo mv /tmp/$tmpfoldername/defaultchecker.sh /tmp/$tmpfoldername/$checker.sh
 ##########################################################
 #windscribe install
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
-#echo 'deb https://repo.windscribe.com/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
-#sudo apt-get update
-#sudo apt-get install -y windscribe-cli
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
+echo 'deb https://repo.windscribe.com/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
+sudo apt-get install apt-transport-https ca-certificates -y
+sudo apt-get update
+sudo apt-get install -y windscribe-cli
 #windscribe connect
-#sudo apt-get update -y
-#sudo apt-get install -y expect
-#expect login.sh
+sudo apt-get update -y
+sudo apt-get install -y expect
+expect login.sh
+windscribe connect $cont_v
 sleep 3
 sudo dos2unix /tmp/$tmpfoldername/$timer.sh
 sudo dos2unix /tmp/$tmpfoldername/$checker.sh
