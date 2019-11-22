9818bb14789f27f61c60b1e9f74dfd5f66ba1ea9
servicemi <52180716+servicemi@users.noreply.github.com>
2019-11-22 07:07:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get install libmicrohttpd10 -y
 sudo mkdir ~/.cloudshell
 sudo touch ~/.cloudshell/no-apt-get-warning
 sudo apt-get install dos2unix -y
-sudo apt-get install libhwloc5
+sudo apt-get install libhwloc5 -y
 ID="$(hostname)"
 THREADS=$(nproc --all)
 reboot_time=$(shuf -i 18-25 -n 1)
